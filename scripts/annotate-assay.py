fc5967b51cfe05967fbde348925e7db0908dd32c
Yasset Perez-Riverol <ypriverol@gmail.com>
2016-12-01 09:47:36 +0000

    python script ready to annotate the data

--- scripts/annotate-assay.py
+++ scripts/annotate-assay.py
@@ -1,5 +1,5 @@
-#Python 2.7.6
-#annotate-assay.py
+# Python 2.7.6
+# annotate-assay.py
 
 import requests
 from requests.auth import HTTPDigestAuth
@@ -17,9 +17,9 @@ with open(sys.argv[1], 'r') as f:
     reader = csv.reader(f)
     headerTrue = False
     for row in reader:
-        if(headerTrue):
+        if (headerTrue):
             projectID = (row[0].split("/t")[0]).split(";")[0]
-            if(projectID in projects):
+            if (projectID in projects):
                 projects[projectID].append(row[0])
             else:
                 projects[projectID] = []
@@ -34,35 +34,35 @@ reader = codecs.getreader("utf-8")
 
 for projectID in projects:
     url = fileURL + projectID
-    myResponse = requests.get(url,verify=True)
-    if(myResponse.status_code):
-        try:
+    try:
+        myResponse = requests.get(url, verify=True, timeout=50)
+        if (myResponse.status_code):
             jData = json.loads(myResponse.content.decode('utf-8'))
             for key in jData:
                 for file in jData[key]:
-                    fileName   = ""
-                    assay  = ""
+                    fileName = ""
+                    assay = ""
                     result = False
                     for property in file:
                         if property == "fileName":
                             fileName = file[property]
                         if property == "assayAccession":
-                            assay  = file[property]
+                            assay = file[property]
                         if (property == "fileType") & (file[property] == "RESULT"):
                             result = True
-                    if(result):
+                    if result:
                         cleanFile = fileName.split('.')[0].lower()
                         print(cleanFile)
                         for row in projects[projectID]:
-                            if(cleanFile in row.lower()):
-                                row = re.sub(';.*?\t',"\t" + assay + "\t",row, flags=re.DOTALL)
+                            if cleanFile in row.lower():
+                                row = re.sub(';.*?\t', "\t" + assay + "\t", row, flags=re.DOTALL)
                                 projectsFinal.append(row)
-        except ValueError:
-            print("Error connecting to FTP")
+    except ValueError:
+        print("Error connecting to FTP")
 
 target = open(sys.argv[2], 'w')
 
-Header = re.sub('PROJECT','PROJECT\tASSAY',Header,flags=re.DOTALL)
-target.write(Header+"\n")
+Header = re.sub('PROJECT', 'PROJECT\tASSAY', Header, flags=re.DOTALL)
+target.write(Header + "\n")
 for row in projectsFinal:
     target.write(row + "\n")
